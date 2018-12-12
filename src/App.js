59acb615cea515866dde266f47b8e797edd968a0
TonyHYK <hungyukao92@gmail.com>
2018-12-12 16:28:35 +0800

    tutorial part0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import _ from "lodash";
+import cx from "classnames";
+import ScatterJS from "scatterjs-core";
+import ScatterEOS from "scatterjs-plugin-eosjs";
+import EOS from "eosjs";
+import "./App.css";
+import { ReactComponent as Circle } from "./circle.svg";
+import { ReactComponent as Times } from "./times.svg";
+
+ScatterJS.plugins(new ScatterEOS());
+const scatter = ScatterJS.scatter;
+
+const network = {
+  blockchain: "eos",
+  protocol: "http",
+  host: "jungle2.cryptolions.io",
+  port: 80,
+  chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473"
+};
+
+const TONY = "tony11111111";
+const JOHN = "john11111111";
 
 class App extends Component {
+  state = {
+    host: TONY,
+    challenger: JOHN,
+    turn: "",
+    board: [],
+    account: null,
+    winner: "none"
+  };
+
+  async componentDidMount() {}
+
+  move = async i => {};
+
   render() {
+    const { account, host, challenger, turn, board, winner } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="title">Tic Tac Toe</div>
+        {account && (
+          <div className="accountName">{`Welcome ${account.name}`}</div>
+        )}
+        <div className="players">
+          <div
+            className={cx("player", { active: turn === host })}
+          >{`Host: ${TONY}`}</div>
+          <div
+            className={cx("player", { active: turn === challenger })}
+          >{`Challenger: ${JOHN}`}</div>
+        </div>
+        <div className="board">
+          {_.range(9).map(i => {
+            return (
+              <div key={i} className="tile" onClick={() => this.move(i)}>
+                {board[i] === 1 ? (
+                  <Circle />
+                ) : board[i] === 2 ? (
+                  <Times />
+                ) : null}
+              </div>
+            );
+          })}
+        </div>
+        <div className="winner">{`Winner: ${winner}`}</div>
       </div>
     );
   }
